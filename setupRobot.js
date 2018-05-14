e335c6dbfcca52dc05896658419a826bdae5905c
TinyUlt <tinyult@icloud.com>
2018-05-14 12:30:49 +0800

    a

--- setupRobot.js
+++ setupRobot.js
@@ -9,130 +9,139 @@ MongoClient.connect(firedCoinInfo.MONGODB, function(err, db) {
     assert.equal(null, err);
     console.log('Connected correctly to server.');
 
-    let servers=[];
-    let binance = (new Binance()).core;
-
-    binance.options({
-        'APIKEY':firedCoinInfo.requestAsksBids.APIKEY,
-        'APISECRET':firedCoinInfo.requestAsksBids.APISECRET
-    });
-
-    let dbchart = db.db(firedCoinInfo.requestAsksBids.name);
     let dbServersManager=db.db(firedCoinInfo.managerName);
+    dbServersManager.dropDatabase(function(err, result){
+        console.log("Error : "+err);
+        console.log("Operation Success ? "+result);
+        // after all the operations with db, close it.
+        let servers=[];
+        let binance = (new Binance()).core;
 
-    // updateAvgMinMaxPrice();
+        binance.options({
+            'APIKEY':firedCoinInfo.requestAsksBids.APIKEY,
+            'APISECRET':firedCoinInfo.requestAsksBids.APISECRET
+        });
 
-    if(firedCoinInfo.SYMBOL === undefined){
-        console.log("SYMBOL undefined");
-        process.exit();
-    }
-    let operatorConfig = JSON.parse(fs.readFileSync(`${firedCoinInfo.SYMBOL}/config_operator.json`));
-    let simulationConfig = JSON.parse(fs.readFileSync(`${firedCoinInfo.SYMBOL}/config_simulation.json`));
+        let dbchart = db.db(firedCoinInfo.requestAsksBids.name);
 
 
-    let managerConfig={};
-    for(let i = 0; i < firedCoinInfo.server.length; i ++){
-        managerConfig[firedCoinInfo.server[i].name] = {};
-        managerConfig[firedCoinInfo.server[i].name].run = firedCoinInfo.server[i].enable;
-        let robotDatabase = db.db(firedCoinInfo.server[i].name);
-        servers[i] = new FServer( firedCoinInfo.server[i],operatorConfig, simulationConfig,db, robotDatabase, dbchart);
-    }
-    setConfig();
-    update();
+        // updateAvgMinMaxPrice();
 
-    function update(){
+        if(firedCoinInfo.SYMBOL === undefined){
+            console.log("SYMBOL undefined");
+            process.exit();
+        }
+        let operatorConfig = JSON.parse(fs.readFileSync(`${firedCoinInfo.SYMBOL}/config_operator.json`));
+        let simulationConfig = JSON.parse(fs.readFileSync(`${firedCoinInfo.SYMBOL}/config_simulation.json`));
 
-        getDepth();
-        updateInfo();
-        getConfig();
-        setTimeout(update,1000);
-    }
-    function getDepth(){
 
-        if (simulationConfig.useManagerAsksBids && simulationConfig.useRealData === 1){
-            binance.depthRequest(firedCoinInfo.SYMBOL,function(error,json){
-                process.stdout.write((new Date()).getSeconds()+' ');
-                if (error === null){
-                    let nowTick = (new Date()).valueOf();
-                    // self.simulTick = nowTick;
+        let managerConfig={};
+        for(let i = 0; i < firedCoinInfo.server.length; i ++){
+            managerConfig[firedCoinInfo.server[i].name] = {};
+            managerConfig[firedCoinInfo.server[i].name].run = firedCoinInfo.server[i].enable;
+            let robotDatabase = db.db(firedCoinInfo.server[i].name);
+            servers[i] = new FServer( firedCoinInfo.server[i],operatorConfig, simulationConfig,db, robotDatabase, dbchart);
+        }
+        setConfig();
+        update();
 
-                    let ask =parseFloat(json.asks[0][0]);
-                    let bid =parseFloat(json.bids[0][0]);
-                    // console.log(ask, bid);
+        function update(){
 
-                    // self.update(symbol,ask, bid);
-                    // callback(nowTick, symbol, ask, bid);
-                    // self.updateAvgMinMaxPrice();
-                    for(let i = 0; i < firedCoinInfo.server.length; i ++){
-                        let server = servers[i];
-                        if(server.GlobalData.simulation !== null){
+            getDepth();
+            updateInfo();
+            getConfig();
+            setTimeout(update,1000);
+        }
+        function getDepth(){
 
-                            server.GlobalData.simulation.recieveDepth(nowTick, firedCoinInfo.SYMBOL, ask, bid);
+            if (simulationConfig.useManagerAsksBids && simulationConfig.useRealData === 1){
+                binance.depthRequest(firedCoinInfo.SYMBOL,function(error,json){
+                    process.stdout.write((new Date()).getSeconds()+' ');
+                    if (error === null){
+                        let nowTick = (new Date()).valueOf();
+                        // self.simulTick = nowTick;
+
+                        let ask =parseFloat(json.asks[0][0]);
+                        let bid =parseFloat(json.bids[0][0]);
+                        // console.log(ask, bid);
+
+                        // self.update(symbol,ask, bid);
+                        // callback(nowTick, symbol, ask, bid);
+                        // self.updateAvgMinMaxPrice();
+                        for(let i = 0; i < firedCoinInfo.server.length; i ++){
+                            let server = servers[i];
+                            if(server.GlobalData.simulation !== null){
+
+                                server.GlobalData.simulation.recieveDepth(nowTick, firedCoinInfo.SYMBOL, ask, bid);
+                            }
+
                         }
-
+                    } else {
+                        console.log(error);
                     }
-                } else {
-                    console.log(error);
+
+                },5);
+            }
+        }
+
+        function updateInfo() {
+            let managerInfo = {};
+            let hasValue = false;
+            for(let i = 0; i < firedCoinInfo.server.length; i ++){
+                let server = servers[i];
+                if(server.infoData !== null){
+                    managerInfo[server.infoData.serverId] = {
+                        run:managerConfig[server.serverInfo.name].run,
+                        buyTradCount:server.infoData.buyTradCount,
+                        sellTradCount:server.infoData.sellTradCount,
+                        robotCount:server.infoData.robotCount,
+                        leftUSDT:server.infoData.realBalance.USDT,
+                        costUSDT:server.infoData.simulDf.USDT,
+                        Property:server.infoData.simulDf.Property,
+                        earnUSDT:server.infoData.earnSum,
+                        updateTime:server.infoData.updateTime,
+                    };
+                    hasValue = true;
                 }
 
-            },5);
-        }
-    }
+            }
+            if(hasValue === true){
+                let where = {_id:0};
+                let updateStr = {$set: managerInfo};
 
-    function updateInfo() {
-        let managerInfo = {};
-        let hasValue = false;
-        for(let i = 0; i < firedCoinInfo.server.length; i ++){
-            let server = servers[i];
-            if(server.infoData !== null){
-                managerInfo[server.infoData.serverId] = {
-                    run:managerConfig[server.serverInfo.name].run,
-                    buyTradCount:server.infoData.buyTradCount,
-                    sellTradCount:server.infoData.sellTradCount,
-                    robotCount:server.infoData.robotCount,
-                    leftUSDT:server.infoData.realBalance.USDT,
-                    costUSDT:server.infoData.simulDf.USDT,
-                    Property:server.infoData.simulDf.Property,
-                    earnUSDT:server.infoData.earnSum,
-                    updateTime:server.infoData.updateTime,
-                };
-                hasValue = true;
+                dbServersManager.collection("info").update(where,updateStr,{upsert:true}, function(err) {
+                    if (err) throw err;
+                });
             }
 
-        }
-        if(hasValue === true){
-            let where = {_id:0};
-            let updateStr = {$set: managerInfo};
 
-            dbServersManager.collection("info").update(where,updateStr,{upsert:true}, function(err) {
+        }
+        function setConfig(){
+
+            let where = {_id:0};
+            let updateStr = {$set: managerConfig};
+
+            dbServersManager.collection("config").update(where,updateStr,{upsert:true}, function(err) {
                 if (err) throw err;
             });
         }
+        function getConfig(){
 
+            dbServersManager.collection("config"). find({_id:0}).toArray(function(err, result) { // 返回集合中所有数据
+                if (err) throw err;
 
-    }
-    function setConfig(){
-
-        let where = {_id:0};
-        let updateStr = {$set: managerConfig};
-
-        dbServersManager.collection("config").update(where,updateStr,{upsert:true}, function(err) {
-            if (err) throw err;
-        });
-    }
-    function getConfig(){
-
-        dbServersManager.collection("config"). find({_id:0}).toArray(function(err, result) { // 返回集合中所有数据
-            if (err) throw err;
-
-            if(result.length !== 0) {
-                managerConfig = result[0];
-                for(let i = 0; i < firedCoinInfo.server.length; i ++){
-                    let server = servers[i];
-                    server.GlobalData.run = managerConfig[server.serverInfo.name].run
+                if(result.length !== 0) {
+                    managerConfig = result[0];
+                    for(let i = 0; i < firedCoinInfo.server.length; i ++){
+                        let server = servers[i];
+                        server.GlobalData.run = managerConfig[server.serverInfo.name].run
+                    }
                 }
-            }
-        });
-    }
+            });
+        }
+    });
+
+
+
 });
 
